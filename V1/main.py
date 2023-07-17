45607f0666f6bee91809faf15ae2eda1e96bf17d
Mystic99K <shashankchodankar5749@gmail.com>
2023-07-17 19:19:40 +0300

    SO AFTER SOME UPDATE TO THE CODE I SEND U THE NEW AND UPDATED CODE I HOPE U GUYS WILL LIKE MY WORK AND STAR AND SHARE THE REPO OR UR MUM DANGER

--- V1/main.py
+++ V1/main.py
@@ -1,54 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 import time
-import requests
-import json
-import tkinter as tk
-from PIL import Image, ImageTk
-from tkinter import ttk
-import csv
-from bs4 import BeautifulSoup
-import math
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+
 ################
 
 PATH = "C:\chromedriver.exe"
-driver = webdriver.Chrome()
 
+options = Options()
+options.add_experimental_option("detach", True)
 
+driver = webdriver.Chrome(options=options)
 
 driver.get('https://www.google.com/travel/flights')
-time.sleep(5)
-a = driver.find_element(By.XPATH, '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div/input''')
+time.sleep(3)
+a = driver.find_element(By.XPATH,
+                        '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div/input''')
 a.clear()
 a.send_keys("GOI")
 time.sleep(1)
-a = driver.find_element(By.XPATH, '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]''')
+a = driver.find_element(By.XPATH,
+                        '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]''')
 ActionChains(driver).click(a).perform()
 
-
-
-a = driver.find_element(By.XPATH, '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input''')
+a = driver.find_element(By.XPATH,
+                        '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input''')
 a.clear()
 a.send_keys("KWI")
 time.sleep(1)
-a = driver.find_element(By.XPATH, '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li''')
+
+a = driver.find_element(By.XPATH,
+                        '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li''')
 ActionChains(driver).click(a).perform()
 
 time.sleep(1)
 
-
-a = driver.find_element(By.XPATH, '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button''')
+a = driver.find_element(By.XPATH,
+                        '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button''')
 ActionChains(driver).click(a).perform()
-
-
-
-
-
-
-
-
