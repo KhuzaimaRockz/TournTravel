65a8b2b4a82c6b4925fd0c54125e53f031f13120
KhuzaimaRockz <bkhuzaima@gmail.com>
2023-07-18 17:33:53 +0300

    updated trip==1, minor mistakes corrected

--- V1/testing.py
+++ V1/testing.py
@@ -39,15 +39,22 @@ if flag_1==True:
         a.clear()
         a.send_keys("GOI")
         time.sleep(1)
-        driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]")
+        a = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]")
         ActionChains(driver).click(a).perform()
 
-        a.send_keys("KWI")
+
         a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input")
         a.clear()
+        a.send_keys("KWI")
         time.sleep(1)
-        driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]")
+        a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]")
         ActionChains(driver).click(a).perform()
+        time.sleep(1)
+
+        a = driver.find_element(By.XPATH,
+                                '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button''')
+        ActionChains(driver).click(a).perform()
+        time.sleep(2)
     elif trip==2:
         a = driver.find_element(By.XPATH,
                                 '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div/input''')
