3a56a7d404099c72c372ff7ef0bffe118c8b1a84
KhuzaimaRockz <bkhuzaima@gmail.com>
2023-07-18 19:35:43 +0300

    added a useless loop to just make the thing run,added changing dates to trip==1 (works fine)

--- V1/testing.py
+++ V1/testing.py
@@ -17,62 +17,86 @@ driver = webdriver.Chrome(options=options)
 driver.get('https://www.google.com/travel/flights')
 time.sleep(3)
 
-a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[1]")
-ActionChains(driver).click(a).perform()
 
-if trip==1:
-    b=driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[2]/ul/li[2]")
-    ActionChains(driver).click(b).perform()
-elif trip==2:
-    c=driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[2]/ul/li[1]")
-    ActionChains(driver).click(c).perform()
-elif trip==3:
-    d=driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[2]/ul/li[3]")
-    ActionChains(driver).click(d).perform()
-else:
+if trip>3:
     print("wrong input, please input the correct option number")
     flag_1=False
 
 if flag_1==True:
-    if trip==1:
-        a = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div/input")
-        a.clear()
-        a.send_keys("GOI")
+    a = driver.find_element(By.XPATH,
+                            "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[1]")
+    time.sleep(1)
+    ActionChains(driver).click(a).perform()
+
+    if trip == 1:
+        b = driver.find_element(By.XPATH,
+                                "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[2]/ul/li[2]")
         time.sleep(1)
-        a = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]")
-        ActionChains(driver).click(a).perform()
+        ActionChains(driver).click(b).perform()
+    elif trip == 2:
+        c = driver.find_element(By.XPATH,
+                                "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[2]/ul/li[1]")
+        time.sleep(1)
+        ActionChains(driver).click(c).perform()
+    elif trip == 3:
+        d = driver.find_element(By.XPATH,
+                                "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[2]/ul/li[3]")
+        time.sleep(1)
+        ActionChains(driver).click(d).perform()
+
+    while flag_1==True:
+        if trip==1:
+            a = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div/input")
+            a.clear()
+            a.send_keys("GOI")
+            time.sleep(1)
+            a = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]")
+            ActionChains(driver).click(a).perform()
 
 
-        a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input")
-        a.clear()
-        a.send_keys("KWI")
-        time.sleep(1)
-        a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]")
-        ActionChains(driver).click(a).perform()
-        time.sleep(1)
+            a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input")
+            a.clear()
+            a.send_keys("KWI")
+            time.sleep(1)
+            a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]")
+            ActionChains(driver).click(a).perform()
+            time.sleep(1)
 
-        a = driver.find_element(By.XPATH,
+            a = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input")
+            ActionChains(driver).click(a).perform()
+            a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div/input")
+            ActionChains(driver).click(a).perform()
+            a.send_keys("27/7/2023")
+            time.sleep(1)
+
+            a = driver.find_element(By.XPATH,"/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[3]/div[3]/div/button")
+            ActionChains(driver).click(a).perform()
+            time.sleep(1)
+
+            a = driver.find_element(By.XPATH,
                                 '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button''')
-        ActionChains(driver).click(a).perform()
-        time.sleep(2)
-    elif trip==2:
-        a = driver.find_element(By.XPATH,
+            ActionChains(driver).click(a).perform()
+            time.sleep(2)
+        elif trip==2:
+            a = driver.find_element(By.XPATH,
                                 '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div/input''')
-        a.clear()
-        a.send_keys("GOI")
-        time.sleep(1)
-        a = driver.find_element(By.XPATH,
+            a.clear()
+            a.send_keys("GOI")
+            time.sleep(1)
+            a = driver.find_element(By.XPATH,
                                 '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]''')
-        ActionChains(driver).click(a).perform()
+            ActionChains(driver).click(a).perform()
 
-        a = driver.find_element(By.XPATH,
+            a = driver.find_element(By.XPATH,
                                 '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input''')
-        a.clear()
-        a.send_keys("KWI")
-        time.sleep(1)
+            a.clear()
+            a.send_keys("KWI")
+            time.sleep(1)
 
-        a = driver.find_element(By.XPATH,
+            a = driver.find_element(By.XPATH,
                                 '''/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li''')
-        ActionChains(driver).click(a).perform()
+            ActionChains(driver).click(a).perform()
 
-        time.sleep(1)
+            time.sleep(1)
+        else:
+            break
