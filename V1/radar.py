4dec591091ec83c6228bd51b07cccb3071f6e5fa
KhuzaimaRockz <bkhuzaima@gmail.com>
2023-09-08 00:02:15 +0300

    fixed selenium and editing

--- V1/radar.py
+++ V1/radar.py
@@ -5,13 +5,15 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from api import get_airport_data
-
-PATH = "C:\chromedriver.exe"
-flag_1 = True
+#PATH = "C:\chromedriver.exe"
+# flag_1 = True
 
 options = Options()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
-
-driver.get('https://www.flightradar24.com')
-time.sleep(2)
+driver = webdriver.Chrome()
+x=2
+driver.get('https://www.flightradar24.com/')
+time.sleep(100)
+a = driver.find_element(By.XPATH, )
+time.sleep(x)
+ActionChains(driver).click(a).perform()
