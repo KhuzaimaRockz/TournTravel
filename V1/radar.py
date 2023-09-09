94b33832a094c8d4db1b8a198886be41a8eb46e5
KhuzaimaRockz <bkhuzaima@gmail.com>
2023-09-09 19:50:57 +0300

    working on radar.py

--- V1/radar.py
+++ V1/radar.py
@@ -4,16 +4,37 @@ import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from api import get_airport_data
 #PATH = "C:\chromedriver.exe"
 # flag_1 = True
 
+u_from = "dxb"
+u_dest = "cdg"
 options = Options()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome()
+driver.implicitly_wait(10)
 x=2
 driver.get('https://www.flightradar24.com/')
-time.sleep(100)
-a = driver.find_element(By.XPATH, )
+#clicking continue with cookies button
+driver.find_element(By.XPATH,'/html/body/div[3]/div[2]/div/div/div[2]/div/div/button').click()
 time.sleep(x)
-ActionChains(driver).click(a).perform()
+
+#search bar
+a = driver.find_element(By.XPATH,'//*[@id="searchBox"]').click()
+time.sleep(x)
+#click flight by route opt
+a = driver.find_element(By.XPATH,'//*[@id="app"]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div[1]/div[2]/a[1]').click()
+time.sleep(x)
+#departure
+a = driver.find_element(By.XPATH,'//*[@id="flight_by_route_from"]').click()
+time.sleep(x)
+a.send_keys(u_from)
+time.sleep(x)
+a = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[2]/div/ul/li[1]/a/span[1]').click()
+#arrival
+a = driver.find_element(By.XPATH,'//*[@id="flight_by_route_to"]').click()
+a.send_keys(u_dest)
+time.sleep(x)
+a = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div[2]/div/div[2]/div/div/div/div/div[2]/div/ul/li/a/span[1]').click()
+
+time.sleep(100)
