0d9b5eb6bc94606f9056f7122dd99373aad7896a
KhuzaimaRockz <bkhuzaima@gmail.com>
2023-09-09 22:21:59 +0300

    radar.py changes from bard

--- V1/radar.py
+++ V1/radar.py
@@ -4,7 +4,8 @@ import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-#PATH = "C:\chromedriver.exe"
+
+# PATH = "C:\chromedriver.exe"
 # flag_1 = True
 
 u_from = "dxb"
@@ -13,37 +14,28 @@ options = Options()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
-x=2
-driver.get('https://www.flightradar24.com/')
-#clicking continue with cookies button
-driver.find_element(By.XPATH,'/html/body/div[3]/div[2]/div/div/div[2]/div/div/button').click()
-time.sleep(x)
 
-#search bar
-a = driver.find_element(By.XPATH,'//*[@id="searchBox"]').click()
-time.sleep(x)
-#click flight by route opt
-a = driver.find_element(By.XPATH,'//*[@id="app"]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div[1]/div[2]/a[1]').click()
-time.sleep(x)
-#departure
-a = driver.find_element(By.XPATH,'//*[@id="flight_by_route_from"]')
-a.send_keys("dxb")
-ActionChains(driver).click(a).perform()
-time.sleep(5)
-a.send_keys(Keys.ENTER)
-ActionChains(driver).click(a).perform()
-a.send_keys(Keys.ENTER)
-ActionChains(driver).click(a).perform()
-time.sleep(5)
-#arrival
-a = driver.find_element(By.XPATH,'//*[@id="flight_by_route_to"]')
-a.send_keys("cdg")
-ActionChains(driver).click(a).perform()
-time.sleep(5)
-a.send_keys(Keys.ENTER)
-ActionChains(driver).click(a).perform()
-time.sleep(5)
-#search
-a = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div[2]/div/div[3]/button')
-ActionChains(driver).click(a).perform()
+driver.get('https://www.flightradar24.com/')
+# clicking continue with cookies button
+driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/div[2]/div/div/button').click()
+time.sleep(2)
+
+# search bar
+driver.find_element(By.XPATH, '//*[@id="searchBox"]').click()
+time.sleep(2)
+
+# click flight by route opt
+driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div[1]/div[2]/a[1]').click()
+time.sleep(2)
+
+# departure
+driver.find_element(By.XPATH, '//*[@id="flight_by_route_from"]').send_keys(u_from)
+time.sleep(2)
+
+# arrival
+driver.find_element(By.XPATH, '//*[@id="flight_by_route_to"]').send_keys(u_dest)
+time.sleep(2)
+
+# search
+driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div[2]/div/div[3]/button').click()
 time.sleep(100)
