7813194379a3d7d861a350ea58d40bfff4e7a570
tariqaq <trqphoenix@gmail.com>
2023-10-09 23:02:37 +0300

    added func for radar

--- V1/toursntravel.py
+++ V1/toursntravel.py
@@ -117,7 +117,7 @@ def ticket():
             print('-'*100)
             usr_from = input('Enter -From- location : ')
             usr_from = get_airport_data(usr_from)
-            
+                        
             usr_dest = input('Enter -Destination- : ')
             usr_dest = get_airport_data(usr_dest)
             
@@ -585,3 +585,54 @@ def ticket():
         print(f"{'Airlines:':^50}{'Ticket Price (KWD):':^50}")
         print(f"{air3.text:^50}{price3.text:^50}")
         print("="*100)
+        radar(usr_from, usr_dest)
+        
+def radar(usr_from, usr_dest):
+    #need to integrate this code to main.py
+    options = Options()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+
+    driver.get('https://www.flightradar24.com/')
+    # clicking continue with cookies button
+    driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]').click()
+    time.sleep(2)
+
+    # search bar
+    driver.find_element(By.XPATH, '//*[@id="searchBox"]').click()
+    time.sleep(2)
+
+    # click flight by route opt
+    driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div[1]/div[2]/a[1]').click()
+    time.sleep(2)
+
+    # departure
+    driver.find_element(By.XPATH, '//*[@id="flight_by_route_from"]').send_keys(usr_from)
+    time.sleep(1)
+    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[2]/div/ul/li/a/span[1]').click()
+    time.sleep(2)
+
+    # arrival
+    driver.find_element(By.XPATH, '//*[@id="flight_by_route_to"]').send_keys(usr_dest)
+    time.sleep(1)
+    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div[2]/div/div[2]/div/div/div/div/div[2]/div/ul/li/a/span[1]').click()
+    time.sleep(2)
+
+    # search
+    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div[2]/div/div[3]/button').click()
+    time.sleep(2)
+
+    #if live aircrafts are available
+    element = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div[1]/h2')
+    search="LIVE FLIGHTS"
+    if search not in element.text:
+        print("No Live Flights")
+    else:
+        driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div/a').click()
+        time.sleep(2)
+        element=driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div/div[2]/a[3]')
+        ActionChains(driver).move_to_element(element).perform()
+        time.sleep(1)
+        driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div/div[2]/a[1]/span').click()
+    time.sleep(100)
